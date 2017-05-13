3510eca579cfdf137da2b94917cea12511585a9e
konrad <konradszk@gmail.com>
2017-05-13 15:53:42 +0200

    próby GA

--- Projekt MOW/Projekt MOW/script.R
+++ Projekt MOW/Projekt MOW/script.R
@@ -1,2 +1,14 @@
-5 + 5
-20 - 10 * 5
+#set.seed(542)
+wymiar_problemu = 10
+func.num <- 8
+
+fitne <- function(a) cec2013(8, rep(0, 10))
+
+GA <- ga(type = "real-valued", fitness = fitne, min = wymiar_problemu, max = wymiar_problemu, popSize = 10 * wymiar_problemu)
+
+#summary(GA)
+#plot(GA)#func.num <- 8
+#D <- 10
+#plot(cec2013(func.num, rep(15, D)))
+
+

5ddf8216d180fda04f94947e38d52483381c94d2
konrad <konradszk@gmail.com>
2017-05-23 11:25:08 +0200

    Działa pakiet GA z wektorami o rozmiarze 10,30,50. Wyswietlanie wykresu i dziennika.

--- Projekt MOW/Projekt MOW/script.R
+++ Projekt MOW/Projekt MOW/script.R
@@ -1,14 +1,31 @@
 #set.seed(542)
-wymiar_problemu = 10
-func.num <- 8
+#wymiar_problemu = 10
+#func.num <- 15
 
-fitne <- function(a) cec2013(8, rep(0, 10))
+#fitne <- function(a) cec2013(func.num, rep(5, 30))
 
-GA <- ga(type = "real-valued", fitness = fitne, min = wymiar_problemu, max = wymiar_problemu, popSize = 10 * wymiar_problemu)
+
+#fitne(5)
 
 #summary(GA)
-#plot(GA)#func.num <- 8
+#plot(GA)
+#func.num <- 8
 #D <- 10
 #plot(cec2013(func.num, rep(15, D)))
 
 
+fx <- function(x) {
+    return(cec2013(8, x))
+}
+GA10 <- ga(type = "real-valued", fitness = fx, min = rep(-100,10), max = rep(100,10), popSize = 100, seed = 1234, monitor = NULL, maxiter = 20)
+
+GA30 <- ga(type = "real-valued", fitness = fx, min = rep(-100, 30), max = rep(100, 30), popSize = 300, seed = 1234, monitor = NULL, maxiter = 30)
+
+GA50 <- ga(type = "real-valued", fitness = fx, min = rep(-100, 50), max = rep(100, 50), popSize = 500, seed = 1234, monitor = NULL, maxiter = 40)
+
+plot(GA10)
+plot(GA30)
+plot(GA50)
+summary(GA10)
+summary(GA30) 
+summary(GA50)

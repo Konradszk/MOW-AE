c3a8dbc4d2298d51bcb3e88bc0a5de82d00d9184
konrad <konradszk@gmail.com>
2017-05-26 17:13:24 +0200

    dodane grupowanie kmeans i pam

--- Projekt MOW/Projekt MOW/script.R
+++ Projekt MOW/Projekt MOW/script.R
@@ -12,32 +12,75 @@
 #func.num <- 8
 #D <- 10
 #plot(cec2013(func.num, rep(15, D)))
-fx1 <- function(x) { return(cec2013(8, x))}
-GA10 <- ga(type = "real-valued",fitness = fx1, min = rep(-100, 10),  max = rep(100, 10),  popSize = 100,   seed = 1234, monitor = NULL, maxiter = 500)
+if (!"GA" %in% row.names(installed.packages()))
+    install.packages("GA")
+library(GA)
+if (!"cluster" %in% row.names(installed.packages()))
+    install.packages("cluster")
+library(cluster)
+if (!"stats" %in% row.names(installed.packages()))
+    install.packages("stats")
+library(stats)
+if (!"doRNG" %in% row.names(installed.packages()))
+    install.packages("doRNG")
+library(doRNG)
+if (!"cec2103" %in% row.names(installed.packages()))
+    install.packages("cec2013")
+library(cec2013)
 
-GA30 <- ga(type = "real-valued", fitness = fx, min = rep(-100, 30), max = rep(100, 30), popSize = 300, seed = 1234, monitor = NULL, maxiter = 30)
+#------------------------------------------------------------------------
+fx <- function(x)
+     {
+    return(cec2013(8, x))
+     }
 
-GA50 <- ga(type = "real-valued", fitness = fx, min = rep(-100, 50), max = rep(100, 50), popSize = 500, seed = 1234, monitor = NULL, maxiter = 40)
+GA10 <- ga(type = "real-valued",fitness = fx, min = rep(-100, 10),  max = rep(100, 10),  popSize = 100,   seed = 1234, monitor = NULL, maxiter = 100,pmutation = 0.5)
+
+GA30 <- ga(type = "real-valued", fitness = fx, min = rep(-100, 30), max = rep(100, 30), popSize = 300, seed = 1234, monitor = NULL, maxiter = 300, pmutation = 0.5)
+
+GA50 <- ga(type = "real-valued", fitness = fx, min = rep(-100, 50), max = rep(100, 50), popSize = 500, seed = 1234, monitor = NULL, maxiter = 500, pmutation = 0.5)
 
 plot(GA10)
-#plot(GA30)
-#plot(GA50)
-summary(GA10)
-summary(GA30)
-summary(GA50)
-#library(ggplot2)
-#ggplot(GA10, aes(Petal.Length, Petal.Width, color = Species)) + geom_point()
+plot(GA30)
+plot(GA50)
+#summary(GA10)
+#summary(GA30)
+#summary(GA50)
+#------------------------------------------------------------------------KMEANS------------------------------------------------------
 
 
-kmeans(GA10@population, 3, iter.max = 10, nstart = 10)
 
-kmeans(GA10@population, 5, iter.max = 10, nstart = 10)
+GA10.km4<-kmeans(GA10@population, 3, iter.max = 100, nstart = 10) #klastry 3, iter 10, poczatkow 10 pkt 
+GA10.km5 <- kmeans(GA10@population, 6, iter.max = 100, nstart = 10)
+GA10.km10 <- kmeans(GA10@population, 10, iter.max = 10, nstart = 20)
 
-kmeans(GA10@population, 10, iter.max = 10, nstart = 20)
+GA30.km4 <- kmeans(GA30@population, 3, iter.max = 100, nstart = 10)  
+GA30.km5 <- kmeans(GA30@population, 6, iter.max = 100, nstart = 10)
+GA30.km10 <- kmeans(GA30@population, 10, iter.max = 10, nstart = 20)
 
-plot(hclust(dist(GA10@population), method = "complete"))
+GA50.km4 <- kmeans(GA50@population, 3, iter.max = 100, nstart = 10) 
+GA50.km5 <- kmeans(GA50@population, 6, iter.max = 100, nstart = 10)
+GA50.km10 <- kmeans(GA50@population, 10, iter.max = 10, nstart = 20)
+
+#--------------------KMEDOID--------------
+
+GA10.kmed3 <- pam(GA10@population, 3, medoids = 3:1)
+GA10.kmed6 <- pam(GA10@population, 6, medoids = 6:1)
+GA10.kmed10 <- pam(GA10@population, 10, medoids = 10:1)
+
+GA30.kmed3 <- pam(GA30@population, 3, medoids = 3:1)
+GA30.kmed6 <- pam(GA30@population, 6, medoids = 6:1)
+GA30.kmed10 <- pam(GA30@population, 10, medoids = 10:1)
+
+GA50.kmed3 <- pam(GA50@population, 3, medoids = 3:1)
+GA50.kmed6 <- pam(GA50@population, 6, medoids = 6:1)
+GA50.kmed10 <- pam(GA50@population, 10, medoids = 10:1)
+
+#--------------------------
+
+#plot(hclust(dist(GA10@population), method = "complete"))
 
 #plot(hclust(dist(GA10@population), method = "average"))
 
-plot(hclust(dist(GA10@population), method = "single"))
+#plot(hclust(dist(GA10@population), method = "single"))
 
